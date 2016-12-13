c9b25864972c0893747d37fa9a034649d46aeaf7
Silmarie <lyra.belacqua4@gmail.com>
2016-12-13 16:29:39 +0000

    * MyTouristWallet.Droid.csproj: Improved interface. AddAmount   improved with error handling.
    * plus.png:
    * MyWallet.xaml:
    * MyTouristWallet.iOS.csproj:
    * MyWallet.xaml.cs:
    * plus.png:
    * arrow.png:
    * Resource.designer.cs:
    * plus.png:
    * plus.png:
    * plus.png:

--- MyTouristWallet/MyWallet.xaml.cs
+++ MyTouristWallet/MyWallet.xaml.cs
@@ -12,16 +12,17 @@ using System.Linq;
 namespace MyTouristWallet
 {
 
-    public partial class MyWallet : ContentPage
-    {
-        static Database database;
-        static List<CurrencyCall> calls = new List<CurrencyCall>();
-        ObservableCollection<Amount> amountList;
-        AbsoluteLayout layout;
+	public partial class MyWallet : ContentPage
+	{
+		static Database database;
+		static List<CurrencyCall> calls = new List<CurrencyCall>();
+		ObservableCollection<Amount> amountList;
+		List<string> currenciesInWallet;
+		AbsoluteLayout layout;
 
-        bool added;
+		bool added;
 
-        Dictionary<string, string> currencies = new Dictionary<string, string>
+		Dictionary<string, string> currencies = new Dictionary<string, string>
 		{
 			{ "EUR", "euro" },
 			{ "USD", "dollar" },
@@ -37,67 +38,75 @@ namespace MyTouristWallet
 			{ "CNY", "chinese yuan renminbi" }
 		};
 
-        Dictionary<string, Color> colors = new Dictionary<string, Color>
-        {
-            { "Aqua", Color.Aqua }, { "Black", Color.Black },
-            { "Blue", Color.Blue }, { "Fuchsia", Color.Fuchsia },
-            { "Gray", Color.Gray }, { "Green", Color.Green },
-            { "Lime", Color.Lime }, { "Maroon", Color.Maroon },
-            { "Navy", Color.Navy }, { "Olive", Color.Olive },
-            { "Purple", Color.Purple }, { "Red", Color.Red },
-            { "Silver", Color.Silver }, { "Teal", Color.Teal },
-            { "White", Color.White }, { "Yellow", Color.Yellow }
-        };
+		Dictionary<string, Color> colors = new Dictionary<string, Color>
+		{
+			{ "Aqua", Color.Aqua }, { "Black", Color.Black },
+			{ "Blue", Color.Blue }, { "Fuchsia", Color.Fuchsia },
+			{ "Gray", Color.Gray }, { "Green", Color.Green },
+			{ "Lime", Color.Lime }, { "Maroon", Color.Maroon },
+			{ "Navy", Color.Navy }, { "Olive", Color.Olive },
+			{ "Purple", Color.Purple }, { "Red", Color.Red },
+			{ "Silver", Color.Silver }, { "Teal", Color.Teal },
+			{ "White", Color.White }, { "Yellow", Color.Yellow }
+		};
 
-        public MyWallet()
-        {
-            InitializeComponent();
+		public MyWallet()
+		{
+			InitializeComponent();
 
-            amountList = new ObservableCollection<Amount>(Database.GetAmounts());
-            walletView.ItemsSource = amountList;
+			amountList = new ObservableCollection<Amount>(Database.GetAmounts());
+			currenciesInWallet = new List<string>();
+			walletView.ItemsSource = amountList;
 
-            added = false;
+			foreach (Amount a in amountList)
+			{
+				currenciesInWallet.Add(a.currency);
+			}
+			added = false;
 
-            foreach (string currencyName in currencies.Keys)
-            {
-                curr.Items.Add(currencyName);
-            }
+			foreach (string currencyName in currencies.Keys)
+			{
+				curr.Items.Add(currencyName);
+			}
 
-            foreach (string currencyName in currencies.Keys)
-            {
-                newCurr.Items.Add(currencyName);
-            }
+			foreach (string currencyName in currencies.Keys)
+			{
+				newCurr.Items.Add(currencyName);
+			}
 
-            foreach (string c in colors.Keys)
-            {
-                colorPicker.Items.Add(c);
-            }
-            colorPicker.SelectedIndexChanged += changeBoxColor;
+			foreach (string c in colors.Keys)
+			{
+				colorPicker.Items.Add(c);
+			}
+			colorPicker.SelectedIndexChanged += changeButtonColor;
+			colorPreview.Clicked += (object s, EventArgs el) =>
+				{
+					colorPicker.Focus();
+				};
+			layout = new AbsoluteLayout();
+			layout.Padding = new Thickness(5, 5, 5, 5);
 
-            layout = new AbsoluteLayout();
-            layout.Padding = new Thickness(5, 5, 5, 5);
+			calculate.Clicked += SumAmounts;
+		}
 
-            calculate.Clicked += SumAmounts;
-        }
+		public static Database Database
+		{
+			get
+			{
+				if (database == null)
+				{
+					database = new Database();
+				}
+				return database;
+			}
+		}
 
-        public static Database Database
-        {
-            get
-            {
-                if (database == null)
-                {
-                    database = new Database();
-                }
-                return database;
-            }
-        }
-
-        void OnSelection(object sender, SelectedItemChangedEventArgs e)
-        {
-            if (e.SelectedItem == null)
-            {
+		void OnSelection(object sender, SelectedItemChangedEventArgs e)
+		{
+			if (e.SelectedItem == null)
+			{
 				return;
-            }
+			}
 
 			editAmount.IsVisible = true;
 
@@ -108,7 +117,7 @@ namespace MyTouristWallet
 				Database.SaveAmount(amountList.ElementAt(index));
 				editAmount.IsVisible = false;
 			};
-        }
+		}
 
 		public void OnEdit(object sender, EventArgs e)
 		{
@@ -134,206 +143,147 @@ namespace MyTouristWallet
 			amountList.Remove(a);
 		}
 
-        void AddAmount(object sender, EventArgs e)
-        {
-            if (added == false)
-            {
-                added = true;
-                newEntry.IsVisible = true;
-            }
-            else
-            {
-                added = false;
-                newEntry.IsVisible = false;
-                decimal nValue = decimal.Parse(amountEntry.Text); //Usar TryParse com msg de erro!!
-                string nCurrency = newCurr.Items[newCurr.SelectedIndex];
-                string nDescription;
-                if (!currencies.TryGetValue(nCurrency, out nDescription))
-                {
-                    // the key isn't in the dictionary.
-                    return; // or whatever you want to do
-                }
-                string nColor = colorPicker.Items[colorPicker.SelectedIndex];
+		void AddAmount(object sender, EventArgs e)
+		{
+			colorPreview.BackgroundColor = Color.Transparent;
+			if (added == false)
+			{
+				added = true;
+				newEntry.IsVisible = true;
+			}
+			else
+			{
+				added = false;
+				decimal nValue;
+
+				msgNewAmount.Text = "Add Amount to wallet";
+
+				if (!decimal.TryParse(amountEntry.Text, out nValue))
+				{
+					msgNewAmount.Text = "No value specified";
+					msgNewAmount.TextColor = Color.Red;
+					return;
+				}
+				string nCurrency = newCurr.Items[newCurr.SelectedIndex];
+				string nDescription;
+				if (!currencies.TryGetValue(nCurrency, out nDescription))
+				{
+					msgNewAmount.Text += "\nNo Currency";
+					msgNewAmount.TextColor = Color.Red;
+					return;
+				}
+
+				string nColor = colorPicker.Items[colorPicker.SelectedIndex];
 				if (nValue != 1)
 					nDescription += "s";
-                Amount a = new Amount(nCurrency, nDescription, nValue, nColor);
-                amountList.Add(a);
-                Database.SaveAmount(a);
-            }
-        }
 
-        void changeBoxColor(object sender, EventArgs e)
-        {
-            Color cPreview;
-            if (!colors.TryGetValue(colorPicker.Items[colorPicker.SelectedIndex], out cPreview))
-            {
-                // the key isn't in the dictionary.
-                return; // or whatever you want to do
-            }
-            colorPreview.Color = cPreview;
-        }
+				if (msgNewAmount.Text == "Add Amount to wallet")
+				{
+					var a = new Amount(nCurrency, nDescription, nValue, nColor);
+					if (currenciesInWallet.IndexOf(a.currency) != -1)
+					{
+						msgNewAmount.Text = "Currency already added";
+						msgNewAmount.TextColor = Color.Red;
+						return;
+					}
+					newEntry.IsVisible = false;
+					currenciesInWallet.Add(a.currency);
+					amountList.Add(a);
+					Database.SaveAmount(a);
+				}
+			}
+		}
 
-       	async void SumAmounts(object sender, EventArgs e)
-        {
-            if (curr.SelectedIndex == -1)
-                return;
-            string targetCurrency = curr.Items[curr.SelectedIndex];
-            decimal sum = decimal.Zero;
-            decimal convertedValue;
-            double pos = 0;
-            layout.Children.Clear();
-            foreach (Amount a in amountList)
-            {
-                Color colorGraph;
-                if (!colors.TryGetValue(a.color, out colorGraph))
-                {
-                    // the key isn't in the dictionary.
-                    return; // or whatever you want to do
-                }
+		void changeButtonColor(object sender, EventArgs e)
+		{
+			Color cPreview = Color.Transparent;
+			if (!colors.TryGetValue(colorPicker.Items[colorPicker.SelectedIndex], out cPreview))
+			{
+				return;
+			}
+			colorPreview.BackgroundColor = cPreview;
+		}
+
+		async void SumAmounts(object sender, EventArgs e)
+		{
+			if (curr.SelectedIndex == -1)
+				return;
+			string targetCurrency = curr.Items[curr.SelectedIndex];
+			decimal sum = decimal.Zero;
+			decimal convertedValue;
+			double pos = 0;
+			layout.Children.Clear();
+			foreach (Amount a in amountList)
+			{
+				Color colorGraph;
+				if (!colors.TryGetValue(a.color, out colorGraph))
+				{
+					return;
+				}
 
 				convertedValue = await ConvertValue(a.AmountValue, a.currency, targetCurrency);
 
-                var box = new BoxView
-                {
-                    Color = colorGraph,
-                    HeightRequest = (double)convertedValue
-                };
-                AbsoluteLayout.SetLayoutBounds(box, new Rectangle(pos, 0, 25, (double)convertedValue));
-                AbsoluteLayout.SetLayoutFlags(box, AbsoluteLayoutFlags.PositionProportional);
+				var box = new BoxView
+				{
+					Color = colorGraph,
+					HeightRequest = (double)convertedValue
+				};
+				AbsoluteLayout.SetLayoutBounds(box, new Rectangle(pos, 0, 25, (double)convertedValue));
+				AbsoluteLayout.SetLayoutFlags(box, AbsoluteLayoutFlags.PositionProportional);
 
-                layout.Children.Add(box);
+				layout.Children.Add(box);
 
-                pos = pos + 0.1;
-                sum = decimal.Add(sum, convertedValue);
-            }
+				pos = pos + 0.1;
+				sum = decimal.Add(sum, convertedValue);
+			}
 
-            walletView.Footer = new ContentView
-            {
-                Content = layout
-            };
-
-            string targetCurrencyDescription;
-            if (!currencies.TryGetValue(targetCurrency, out targetCurrencyDescription))
-            {
-                // the key isn't in the dictionary.
-                return; // or whatever you want to do
-            }
-            inText.IsVisible = false;
-            totalAmount.Text = "You have " + Math.Round(sum, 2) + " " + targetCurrencyDescription + " total";
-        }
-
-        public static async Task<decimal> ConvertValue(decimal value, string firstCurrency, string secondCurrency)
-        {
-            string url = "http://download.finance.yahoo.com/d/quotes?f=sl1d1t1&s=" +
-                firstCurrency + secondCurrency + "=X";
-            string result = "";
-            CurrencyCall conversion = new CurrencyCall("", 0, "", "");
-
-            try
-            {
-
-                var httpClient = new HttpClient();
-                httpClient.Timeout = TimeSpan.FromSeconds(5);
-                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, url);
-                var response = await httpClient.SendAsync(request);
-                result = await response.Content.ReadAsStringAsync();
-                string[] answer = result.Split(',');
-                conversion = new CurrencyCall(answer[0].Replace("\"", ""), decimal.Parse(answer[1]), answer[2].Replace("\"", ""), answer[3].Replace("\"", "").Replace("\n", ""));
-                //save time by saving currency rates
-                calls.Add(conversion);
-                Database.SaveCurrencyCall(conversion);
-            }
-            catch (Exception exception)
-            {
-                string currenciesToConvert = firstCurrency + secondCurrency + "=X";
-                Debug.WriteLine("OFFLINE CONVERSION");
-				Debug.WriteLine(exception);
-                conversion = Database.GetCurrencyCallRate(currenciesToConvert).ElementAt(0);
-            }
-
-            return decimal.Multiply(conversion.rate, value);
-        }
-
-        public Task<string> InputBox(INavigation navigation, Amount am)
-        {
-            // wait in this proc, until user did his input 
-            var tcs = new TaskCompletionSource<string>();
-
-            var lblTitle = new Label { Text = "Change quantity", HorizontalOptions = LayoutOptions.Center, FontAttributes = FontAttributes.Bold };
-            var lblMessage = new Label { Text = "Enter new quantity:" };
-			var txtInput = new Entry { Text = "" + am.AmountValue };
-
-            var btnOk = new Button
-            {
-                Text = "Ok",
-                WidthRequest = 100,
-                BackgroundColor = Color.FromRgb(0.8, 0.8, 0.8),
-            };
-            btnOk.Clicked += async (s, e) =>
-            {
-                // close page
-                var result = txtInput.Text;
-                await navigation.PopModalAsync();
-                // pass result
-                tcs.SetResult(result);
-            };
-
-            var btnCancel = new Button
-            {
-                Text = "Cancel",
-                WidthRequest = 100,
-                BackgroundColor = Color.FromRgb(0.8, 0.8, 0.8)
-            };
-            btnCancel.Clicked += async (s, e) =>
-            {
-                // close page
-                await navigation.PopModalAsync();
-                // pass empty result
-                tcs.SetResult(null);
-            };
-
-            var btnDelete = new Button
-            {
-                Text = "Delete",
-                WidthRequest = 100,
-                BackgroundColor = Color.FromRgb(0.8, 0.8, 0.8)
-            };
-            btnDelete.Clicked += async (s, e) =>
-            {
-                database.DeleteAmount(am.ID);
-                amountList.Remove(am);
-                // close page
-                await navigation.PopModalAsync();
-                // pass empty result
-                tcs.SetResult(null);
-            };
-
-            var slButtons = new StackLayout
-            {
-                Orientation = StackOrientation.Horizontal,
-                Children = { btnDelete, btnOk, btnCancel },
-            };
-
-
-			// create and show page
-			var page = new ContentPage();
-            page.Content = new StackLayout
+			walletView.Footer = new ContentView
 			{
-				Padding = new Thickness(0, 40, 0, 0),
-				VerticalOptions = LayoutOptions.StartAndExpand,
-				HorizontalOptions = LayoutOptions.CenterAndExpand,
-				Orientation = StackOrientation.Vertical,
-				Children = { lblTitle, lblMessage, txtInput, slButtons },
+				Content = layout
 			};
-            navigation.PushModalAsync(page);
-            // open keyboard
-            txtInput.Focus();
 
-            // code is waiting her, until result is passed with tcs.SetResult() in btn-Clicked
-            // then proc returns the result
-            return tcs.Task;
-        }
-    }
+			string targetCurrencyDescription;
+			if (!currencies.TryGetValue(targetCurrency, out targetCurrencyDescription))
+			{
+				// the key isn't in the dictionary.
+				return; // or whatever you want to do
+			}
+			inText.IsVisible = false;
+			totalAmount.Text = "You have " + Math.Round(sum, 2) + " " + targetCurrencyDescription + " total";
+		}
+
+		public static async Task<decimal> ConvertValue(decimal value, string firstCurrency, string secondCurrency)
+		{
+			string url = "http://download.finance.yahoo.com/d/quotes?f=sl1d1t1&s=" +
+				firstCurrency + secondCurrency + "=X";
+			string result = "";
+			var conversion = new CurrencyCall("", 0, "", "");
+
+			try
+			{
+
+				var httpClient = new HttpClient();
+				httpClient.Timeout = TimeSpan.FromSeconds(5);
+				var request = new HttpRequestMessage(HttpMethod.Get, url);
+				var response = await httpClient.SendAsync(request);
+				result = await response.Content.ReadAsStringAsync();
+				string[] answer = result.Split(',');
+				conversion = new CurrencyCall(answer[0].Replace("\"", ""), decimal.Parse(answer[1]), answer[2].Replace("\"", ""), answer[3].Replace("\"", "").Replace("\n", ""));
+				//save time by saving currency rates
+				calls.Add(conversion);
+				Database.SaveCurrencyCall(conversion);
+			}
+			catch (Exception exception)
+			{
+				string currenciesToConvert = firstCurrency + secondCurrency + "=X";
+				Debug.WriteLine("OFFLINE CONVERSION");
+				Debug.WriteLine(exception);
+				conversion = Database.GetCurrencyCallRate(currenciesToConvert).ElementAt(0);
+			}
+
+			return decimal.Multiply(conversion.rate, value);
+		}
+	}
 
 
 }
